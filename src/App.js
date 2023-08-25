3819dc6c6989d23b11c37114cbbd5c61d4d8c2f2
shivbanarasi <shivbanarasi0542@gmail.com>
2023-08-25 20:20:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenceItems from "./components/ExpenceItems";
+function App() {
+  return (
+    <div>
+      <h1>lets start</h1>
+  <ExpenceItems/>
+    </div>
+  
+  )
+}
+
+export default App;
---
+++ src/components/ExpenceItems.js
@@ -0,0 +1,12 @@
+function ExpenceItems(){
+return(
+    <div>
+        <h1>Expence Items</h1>
+        <h5>Food Rs 10</h5>
+        <h5>patrol Rs 100</h5>
+        <h5>movies Rs 200</h5>
+    </div>
+
+);
+}
+export default ExpenceItems;
