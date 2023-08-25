48d9da4877d24e5c3f6b3c757b8161158b7dee4b
shivbanarasi <shivbanarasi0542@gmail.com>
2023-08-25 20:58:10 +0530

    adding javascript in react

--- src/components/ExpenceItems.js
+++ src/components/ExpenceItems.js
@@ -1,13 +1,15 @@
 import './ExpenceItem.css';
 function ExpenceItems(){
+    const list=["Food","petrol","movies"];
+    const price=[10,100,200];
 return(
     <div className='expItem'>
         <div>
         <h1>Expence Items</h1>
-        <div><h5 id='itemlist'>Food Rs 10</h5></div>
+        <div><h5 id='itemlist'>{list[0]} Rs {price[0]}</h5></div>
         
-       <div><h5 id='itemlist'>patrol Rs 100</h5></div> 
-       <div><h5 id='itemlist'>movies Rs 200</h5></div> 
+       <div><h5 id='itemlist'>{list[1]} Rs {price[1]}</h5></div> 
+       <div><h5 id='itemlist'>{list[2]} Rs {price[2]}</h5></div> 
         </div>
         
     </div>
