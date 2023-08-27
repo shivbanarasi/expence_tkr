b54593228deeff2451081b7b567f5a5025aa28ff
shivbanarasi <shivbanarasi0542@gmail.com>
2023-08-27 16:22:22 +0530

    pass by loop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import ExpenceItems from "./components/ExpenceItems";
 function App() {
-  return (
-    <div>
-      <h1>lets start</h1>
-  <ExpenceItems/>
-    </div>
+   const list=[];
   
+  const expencelist=[
+    {
+      title:"food",
+      price:30,
+      location:"delhi"
+    },
+    {
+      title:"fuel",
+      price:100,
+      location:"palika bazar,delhi"
+    },
+    {
+      title:"movie",
+      price:200,
+      location:"pvr cinemas"
+    }
+  ]
+
+  for(let i=0;i<=expencelist.length-1;i++){
+list.push(<ExpenceItems 
+  title={expencelist[i].title} 
+  price={expencelist[i].price} 
+  location={expencelist[i].location}>
+  </ExpenceItems>)
+  }
+
+  return (
+      <div>
+      <h1>lets start</h1>
+  <ul>{list}</ul>
+    </div>
   )
 }
 
