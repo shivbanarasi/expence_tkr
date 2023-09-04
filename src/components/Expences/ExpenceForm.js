d8b09e2111d98a521f14022588ff25075475a4ac
shivbanarasi <shivbanarasi0542@gmail.com>
2023-09-04 15:28:05 +0530

    submit form

--- src/components/Expences/ExpenceForm.js
+++ src/components/Expences/ExpenceForm.js
@@ -1,17 +1,30 @@
 import "./ExpenceForm.css"
 import { useState } from "react"
 const ExpenceForm=()=>{
+   // const data=[];
 
-    const conso=(e)=>{
-        e.preventDefault()
-        const title=document.getElementById("title").value;
-        const price=document.getElementById("amount").value;
-        const location=document.getElementById("location").value
-        console.log(title,price,location)
-    }
+   
     const [enteredTitle,setTitle]=useState('')
     const [enteredAmount,setAmount]=useState('')
     const [enteredDate,setDate]=useState('')
+
+    const conso=(e)=>{
+        e.preventDefault()
+
+        const expenceData={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date:enteredDate
+        }
+        console.log(expenceData)
+        // const date=document.getElementById("date")
+        // const title=document.getElementById("title").value;
+        // const price=document.getElementById("amount").value;
+        // const location=document.getElementById("location").value
+        // data.push({title:title,price:price,date:date})
+        // console.log(title,price,location)
+        // console.log(data)
+    }
 
     const changeTitle=(e)=>{
         setTitle(e.target.value)
@@ -30,10 +43,8 @@ return(
         <input type="text" placeholder="expence title" onChange={changeTitle} id="title"/>
         <label>amount:</label>
         <input type="text" placeholder="amount" onChange={changeAmount} id="amount"/>
-        <label>location:</label>
-        <input type="text" placeholder="location" onChange={changeDate} id="location"/>
         <label>Date:</label>
-        <input type="Date"/>
+        <input type="Date" id="date" onChange={changeDate}/>
         <br/>
         <button onClick={conso}>add expence</button>
     </form>
